55bdd511d10b518c10af2ba6edb3846c8b5fe1dd
Jérôme Augé <jerome.auge@anakeen.com>
2016-04-11 09:43:54 +0200

    refactor(license): Changed license to GNU Lesser General Public License v3.0 (refs #6278)

--- Class/Class.DCalendar.php
+++ Class/Class.DCalendar.php
@@ -6,7 +6,6 @@ namespace Dcp\Freeevent;
  * Dynamic calendar methods
  *
  * @author Anakeen
- * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
  * @package FREEEVENT
 */
 class Dcalendar extends \Dcp\Family\Dsearch
--- Zone/calvcard.php
+++ Zone/calvcard.php
@@ -3,7 +3,6 @@
  * Generated Header (not documented yet)
  *
  * @author Anakeen
- * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html GNU Affero General Public License
  * @package FREEEVENT
 */
 
